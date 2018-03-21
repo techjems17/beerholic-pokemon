c2d68f76eab66ab35bfd90fb8f685ff1a598a561
Jordan Smith <techjems17@gmail.com>
2018-03-21 11:41:02 -0600

    updated index template

--- poke-beer-app/app/controllers/poke.js
+++ poke-beer-app/app/controllers/poke.js
@@ -1,4 +1,12 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions: {
+      updateCurrentPoke(value) {
+        var currentPoke = this.get("model.beerList").filter(beer => {
+          return beer.id == value;
+        });
+        this.set("currentPoke", currentPoke);
+      }
+    }
 });
