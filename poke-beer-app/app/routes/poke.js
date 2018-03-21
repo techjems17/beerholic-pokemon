a213e89279a2982f8a30427fbf36d2acc891f1b7
Jordan Smith <techjems17@gmail.com>
2018-03-21 10:43:03 -0600

    added controller

--- poke-beer-app/app/routes/poke.js
+++ poke-beer-app/app/routes/poke.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+import $ from 'jquery';
 
 export default Route.extend({
+    model() {
+        return hash({
+            pokemon: $.getJSON("https://cors-anywhere.herokuapp.com/http://pokeapi.co/api/v2/pokemon/"),
+            beerList: $.getJSON('beer.json')
+        })
+    },
 });
